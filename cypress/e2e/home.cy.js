652c29b03ceece1d3ff131c00d4237a411ab8de9
cristian.ataliba <cristian.ataliba@outlook.com>
2025-04-12 22:54:37 -0300

    Treinamento Cypress com aplicação MarkL consumindo API

--- cypress/e2e/home.cy.js
+++ cypress/e2e/home.cy.js
@@ -1,8 +1,25 @@
-describe('Home', () => {
-  it('Web App deve estar on line', () => {
-    cy.visit('http://localhost:3000')
-    cy.title().should('eq', 'Gerencie suas tarefas com Mark L')
 
+describe('tarefas', () => {
+  it('Metodo recrsivo para excluir e cadastrar uma tafefa', () => {
+    cy.request({
+      url: 'http://localhost:3333/helper/tasks',
+      method: 'delete',
+      body: {name: 'Ler um livro bom de node.js'}
+    }).then(response =>{
+      expect(response.status).to.equal(204)
+
+    })
+
+
+
+
+    cy.visit('http://localhost:3000')
+    cy.get('input[placeholder="Add a new Task"]')
+      .type("Ler um livro bom de node.js")
+    cy.contains('button', 'Create')
+      .click()
+    cy.contains('Ler um livro bom de node.js')
+    .should('be.visible')
   });
 });
 
