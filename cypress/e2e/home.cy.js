ae52ab7f9f5ca6d78b8f009aa688ea79689ee634
cristian.ataliba <cristian.ataliba@outlook.com>
2025-04-20 12:39:58 -0300

    Implementação de Custom Commands no Cypress

--- cypress/e2e/home.cy.js
+++ cypress/e2e/home.cy.js
@@ -1,66 +1,22 @@
-describe('tarefas', () => {
-  it('Metodo recursivo para excluir e cadastrar uma tafefa', () => {
+describe('Gerenciamento de tarefas', () => {
 
-    const taskNameCenario1 = 'Ler um livro bom de node.js'
-
-    cy.request({
-      url: 'http://localhost:3333/helper/tasks',
-      method: 'delete',
-      body: {name: taskNameCenario1}
-    }).then(response =>{
-      expect(response.status).to.equal(204)
-
-    })
-
-    cy.createTask(taskNameCenario1)
-
-    cy.contains(taskNameCenario1)
-    .should('be.visible')
-  });
-
-  it('Não deve cadastrar tarefas duplicadas', () => {
-
-    const taskNameCenario2 = {
-      name: 'Estudar JavaScript',
-      is_done: 'false'
-    }
-
-    cy.request({
-      url: 'http://localhost:3333/helper/tasks',
-      method: 'delete',
-      body: {name: taskNameCenario2.name}
-    }).then(response => {
-      expect(response.status).to.equal(204)
-    })
-
-    //Dado que eu cadastro uma tarefa
-    cy.request({
-      url: 'http://localhost:3333/tasks',
-      method: 'post',
-      body: taskNameCenario2
-    }).then(response => {
-      expect(response.status).to.equal(201)
-    })
-
-    // Quando eu faço o cadastro da mesma tarefa já existente
-   cy.createTask(taskNameCenario2.name)
-
-    //Então eu valido se essa tarefa já exite pela label de alerta  
-    cy.get('.swal2-html-container')
-      .should('be.visible')
-      .should('have.text', 'Task already exists!')
-  });
-
-//Função customizada
-  Cypress.Commands.add('createTask', (taskName)=> {
-    cy.visit('http://localhost:3000')
-    cy.get('input[placeholder="Add a new Task"]')
-      .type(taskName)
-    cy.contains('button', 'Create')
-      .click()
-
+  it('Deve excluir e cadastrar uma tarefa com sucesso', () => {
+    const task = 'Ler um livro bom de node.js'
+    cy.removeTaskByName(task)
+    cy.createTask(task)
+    cy.contains(task).should('be.visible')
   })
 
+  it('Não deve permitir cadastro de tarefas duplicadas', () => {
+    const task = {
+      name: 'Estudar JavaScript',
+      is_done: false
+    }
 
-});
+    cy.removeTaskByName(task.name)
+    cy.postTask(task)
+    cy.createTask(task.name)
+    cy.contains('Task already exists!').should('be.visible')
+  })
 
+})
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,25 +1,26 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+Cypress.Commands.add('createTask', (task)=> {
+    cy.visit('http://localhost:3000')
+    cy.get('input[placeholder="Add a new Task"]').type(task)
+    cy.contains('button', 'Create').click()
+  })
+
+Cypress.Commands.add('removeTaskByName', (task)=>{
+    cy.request({
+        url: 'http://localhost:3333/helper/tasks',
+        method: 'delete',
+        body: {name: task}
+      }).then(response => {
+        expect(response.status).to.equal(204)
+      })
+
+Cypress.Commands.add('postTask', (task)=>{
+    cy.request({
+        url: 'http://localhost:3333/tasks',
+        method: 'post',
+        body: task
+      }).then(response => {
+        expect(response.status).to.equal(201)
+      })
+ })
+})
+  
