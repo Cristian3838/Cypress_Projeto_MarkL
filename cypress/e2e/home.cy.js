be52c1ba4a146659d961130cf8be4415ec158578
cristian.ataliba <cristian.ataliba@outlook.com>
2025-04-26 21:12:46 -0300

    Treinamento Cypress com aplicação MarkL Entendendo o Context e usando o Find

--- cypress/e2e/home.cy.js
+++ cypress/e2e/home.cy.js
@@ -1,27 +1,40 @@
 describe('Gerenciamento de tarefas', () => {
 
-  it('Deve excluir e cadastrar uma tarefa com sucesso', () => {
-    const task = 'Ler um livro bom de node.js'
-    cy.removeTaskByName(task)
-    cy.createTask(task)
-    cy.contains(task).should('be.visible')
+  context('Cadastro', ()=>{
+    it('Deve excluir e cadastrar uma tarefa com sucesso', () => {
+      const task = 'Ler um livro bom de node.js'
+      cy.removeTaskByName(task)
+      cy.createTask(task)
+      cy.contains(task).should('be.visible')
+    })
+  
+    it('Não deve permitir cadastro de tarefas duplicadas', () => {
+      const task = {
+        name: 'Estudar JavaScript',
+        is_done: false
+      }
+  
+      cy.removeTaskByName(task.name)
+      cy.postTask(task)
+      cy.createTask(task.name)
+      cy.contains('Task already exists!').should('be.visible')
+    })
+  
+    it('Validar campo em branco', () => {
+      cy.createTask()
+      cy.isRequired('This is a required field')
+    });
   })
 
-  it('Não deve permitir cadastro de tarefas duplicadas', () => {
-    const task = {
-      name: 'Estudar JavaScript',
-      is_done: false
-    }
-
-    cy.removeTaskByName(task.name)
-    cy.postTask(task)
-    cy.createTask(task.name)
-    cy.contains('Task already exists!').should('be.visible')
+  context('Atualização', ()=>{
+    it('Deve concluir uma tarefa', () => {
+      const task = {
+        name: 'Pagar conta de consumo',
+        is_done: false
+      }
+      cy.removeTaskByName(task.name)
+      cy.postTask(task)
+      cy.validaTaskMarcada(task)
+    })
   })
-
-  it('Validar campo em branco', () => {
-    cy.createTask()
-    cy.isRequired('This is a required field')
-  });
-
 })
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -4,8 +4,7 @@ Cypress.Commands.add('createTask', (task = '')=> {
     cy.get('input[placeholder="Add a new Task"]').as('inputTask')
 
     if(task !== ''){
-    cy.get('@inputTask')
-      .type(task)
+    cy.get('@inputTask').type(task)
     } 
 
     cy.contains('button', 'Create').click()
@@ -39,5 +38,16 @@ Cypress.Commands.add('postTask', (task)=>{
         expect(response.status).to.equal(201)
       })
  })
-})
+
+ Cypress.Commands.add('validaTaskMarcada', (task)=>{
+  cy.visit('http://localhost:3000')
+      cy.contains("p", task.name) // Encontrar o texto dentro do <p> com a classe
+      .parent() // Navegar para o elemento pai (o <div>)
+      .find('button[class*= "_listItemToggle" ]') // Encontrar o botão dentro do <div> (se houver)
+      .click() // Realizar o clique na imagem (ou outro elemento, caso precise)
+      cy.contains('p', task.name)
+      .should('have.css', 'text-decoration-line', 'line-through')
+    });
+ })
+
   
