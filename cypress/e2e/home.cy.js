bd187b00c1f106e957b0a79e53f525d31a70c236
cristian.ataliba <cristian.ataliba@outlook.com>
2025-05-03 19:53:30 -0300

    Treinamento Cypress com aplicação MarkL: Fixture, Variaveis

--- cypress/e2e/home.cy.js
+++ cypress/e2e/home.cy.js
@@ -1,19 +1,23 @@
 describe('Gerenciamento de tarefas', () => {
 
+  let testData;
+
+  before(()=>{
+    cy.fixture('tasks').then(massa =>{
+      testData = massa
+    })
+  })
+
   context('Cadastro', ()=>{
     it('Deve excluir e cadastrar uma tarefa com sucesso', () => {
-      const task = 'Ler um livro bom de node.js'
-      cy.removeTaskByName(task)
-      cy.createTask(task)
-      cy.contains(task).should('be.visible')
+      const task = testData.cadastrarTarefa
+      cy.removeTaskByName(task.name)
+      cy.createTask(task.name)
+      cy.contains(task.name).should('be.visible')
     })
   
     it('Não deve permitir cadastro de tarefas duplicadas', () => {
-      const task = {
-        name: 'Estudar JavaScript',
-        is_done: false
-      }
-  
+      const task = testData.duplicadaVerifica
       cy.removeTaskByName(task.name)
       cy.postTask(task)
       cy.createTask(task.name)
@@ -28,10 +32,7 @@ describe('Gerenciamento de tarefas', () => {
 
   context('Atualização', ()=>{
     it('Deve concluir uma tarefa', () => {
-      const task = {
-        name: 'Pagar conta de consumo',
-        is_done: false
-      }
+      const task = testData.validaTaskMarcada
       cy.removeTaskByName(task.name)
       cy.postTask(task)
       cy.validaTaskMarcada(task)
@@ -40,10 +41,7 @@ describe('Gerenciamento de tarefas', () => {
 
   context('Exclusão', ()=>{
     it('Deve remover uma tarefa', () => {
-      const task = {
-        name: 'Estudar JavaScript',
-        is_done: false
-      }
+      const task = testData.removerTarefa
       cy.removeTaskByName(task.name)
       cy.postTask(task)
       cy.removerTarefa(task)
